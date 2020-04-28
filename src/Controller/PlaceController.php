c8e3a05d65c3c891df84981ae5877f8956c1055a
CamVercruysse <61409842+CamVercruysse@users.noreply.github.com>
2020-04-28 10:06:39 +0200

    Filtres des categories dans la liste des places

--- src/Controller/PlaceController.php
+++ src/Controller/PlaceController.php
@@ -2,12 +2,14 @@
 
 namespace App\Controller;
 
+use App\Data\SearchData;
+use App\Form\SearchFormType;
 use App\Service\PlaceService;
 use App\Service\BackgroundService;
 use App\Repository\PlaceRepository;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-
 
 class PlaceController extends AbstractController
 {
@@ -15,14 +17,14 @@ class PlaceController extends AbstractController
     
     private $placeRepository;
 
- 
     private $backgroundService;
 
-    public function __construct( PlaceService $placeService, PlaceRepository $placeRepository, BackgroundService $backgroundService){
-        $this->placeService = $placeService;
-        $this->placeRepository = $placeRepository;
 
+    public function __construct( PlaceService $placeService, PlaceRepository $placeRepository, BackgroundService $backgroundService ){
+        $this->placeService      = $placeService;
+        $this->placeRepository   = $placeRepository;
         $this->backgroundService = $backgroundService;
+        // $this->request = $request;
         
     }
 
@@ -30,15 +32,22 @@ class PlaceController extends AbstractController
     /**
      * @Route("/list", name="list")
      */
-    public function list()
+    public function list(Request $request)
     {
-        $places = $this->placeRepository->findAll();
-
+        $data = new SearchData();
+        $form = $this->createForm(SearchFormType::class, $data);
+        $form->handleRequest($request);
+        // dd($data); 
+        //$places = $this->placeRepository->findAll();
+        $places = $this->placeRepository->findSearch($data);
+        
         return $this->render('place/list.html.twig', [
-            'places' => $places,
-            'background' => $this->backgroundService->getBackgroundRandom( )
+            'places'     => $places,
+            'form'       => $form->createView(),
+            'background' => $this->backgroundService->getBackgroundRandom()
         ]);
     }
+
 
     /**
      * @Route("/list/{id}", name="show", requirements={"id"="\d+"})
@@ -46,9 +55,25 @@ class PlaceController extends AbstractController
     public function show()
     {
         return $this->render( 'place/show.html.twig', array(
-            'background' => $this->backgroundService->getbackgroundRandom(  ),
+            'background' => $this->backgroundService->getbackgroundRandom(),
         ));
     }
 
+    /**
+     * @Route("/list", name="list")
+     */
+/*     public function searchFilter(Request $request)
+    {
+        
+        $data   = new SearchData();
+        $places = $this->placeRepository->findSearch($data);
+        $form   = $this->createForm(SearchFormType::class, $data);
+        $form->handleRequest($request);
+
+        return $this->render('place/list.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+ */
    
 }
