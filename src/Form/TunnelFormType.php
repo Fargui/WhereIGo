9ef6288887fbc258a2eca9220a5a5ffd13d091c6
Fargui <gui.dumez@gmail.com>
2020-05-04 18:22:00 +0200

    fix doublons

---
+++ src/Form/TunnelFormType.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Reponse;
+use App\Data\TunnelData;
+use App\Entity\Question;
+use Doctrine\ORM\EntityRepository;
+use Symfony\Component\Form\FormEvent;
+use App\Repository\QuestionRepository;
+use Symfony\Component\Form\FormEvents;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+
+class TunnelFormType extends AbstractType {
+
+   
+
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+
+            ->add('question', EntityType::class, [
+                'class' => Question::class,  
+                'mapped' => false,
+                'query_builder' => function (EntityRepository $er) {
+                    return $er->createQueryBuilder('q')
+                    ->orderBy('rand()');  
+                    dump($er);
+                }
+            ])
+            ->add('reponses', EntityType::class, [
+                'class' => Reponse::class, 
+                'expanded' => true,
+                'label'  =>false,
+                'multiple'  => false,
+            ])
+           ;
+         $builder->get('question')->addEventListener(
+             FormEvents::POST_SUBMIT,
+             function(FormEvent $event){
+                dump($event->getForm());
+                dump($event->getForm()->getData()->getAnswer());
+                $reponse = $event->getForm()->getData()->getAnswer();
+             }
+            );
+    }
+
+    public function getBlockPrefix()
+    {
+        return '';
+    }
+}
---
+++ src/Service/TunnelService.php
@@ -0,0 +1,25 @@
+<?php
+namespace App\Service;
+
+
+use App\Repository\QuestionRepository;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+
+class TunnelService{
+    
+
+    private $questionRepository;
+
+    public function __construct( QuestionRepository $questionRepository, SessionInterface $session){
+        $this->questionRepository = $questionRepository;
+        $this->session = $session;
+    }
+
+   public function searchQuestion($data){
+       return $this->questionRepository->searchQuestion($data);   
+   }
+
+   public function getQuestionRandom(){
+    return $this->questionRepository->getQuestionRandom();   
+}
+}
